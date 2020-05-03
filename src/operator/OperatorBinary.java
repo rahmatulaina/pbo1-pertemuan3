68109b675f358519931f8e9a1d34cc16bc28309a
rahmatulaina <rahmatulaina28@gmail.com>
2020-05-03 09:44:27 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Rahmatul Aina";
+    int jumlahSks= 24;
+    double indeksPrestasiKumulatif = 3.89;
+    
+}
